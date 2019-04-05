379d4b600e515cc8cb29c7a5a534ca1a3f748c1b
fadetime <fadetime@163.com>
2019-04-05 11:25:50 +0800

    alphaV0.21.1
    修改客服退菜地址

--- src/assets/js/config.js
+++ src/assets/js/config.js
@@ -2,9 +2,9 @@ let dev = '//192.168.1.5:3000'
 let product = '//api.ebuymart.sg'
 let pro = '//18.136.155.152:3000'
 let customerService_dev = '//13.250.234.103:4000'
-let customerService_pro = '//2.1.39.240:4000'
+let customerService_pro = '//3.1.39.240:4000'
 
 export default {
-   server : dev,
-   customerServiceAddress:customerService_dev
+   server : pro,
+   customerServiceAddress:customerService_pro
 }
